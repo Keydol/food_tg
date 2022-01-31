ef12a5ccf7264e8b0ef062b1a0b83b310d38af61
Keydol <stockiiarsen@gmail.com>
2022-01-31 23:41:47 +0200

    change id

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 BOT_TOKEN = "5297019034:AAHhrkTclZG0qBaXV5dCnE9AvPQWDAqPC-s"
 time_to_eat = "11:30"
-chat_id = -1001339947521
+chat_id = -489162852 #Datawiz BES
 message_template = (
     "Замовлення\n"
     "{order}\n\n"
