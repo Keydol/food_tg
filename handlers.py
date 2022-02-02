a7c9f905ee4e758e574eee4d98ab1bd4ebe8cc74
Keydol <stockiiarsen@gmail.com>
2022-02-02 10:21:53 +0200

    add random

--- handlers.py
+++ handlers.py
@@ -1,5 +1,6 @@
 import datetime
 import pytz
+import random
 
 import settings
 
@@ -10,7 +11,7 @@ from telegram.ext import CallbackContext
 def generate_order_message(context: CallbackContext):
     i = 0
     text = ""
-    for user, order in context.chat_data["order"].items():
+    for order in context.chat_data["order"].values():
         i += 1
         text += f"{i}. {order}\n"
     return settings.message_template.format(order=text)
@@ -25,12 +26,8 @@ def edit_order_message(update: Update, context: CallbackContext):
 
 
 def add_order(update: Update, context: CallbackContext):
-    if update.effective_message.reply_to_message.text.startswith("Нове"):
-        for key in context.chat_data.copy().keys():
-            del context.chat_data[key]
-        context.chat_data["message_id"] = update.effective_message.reply_to_message.message_id
-        context.chat_data["order"] = {}
     context.chat_data["order"][update.effective_user.id] = update.message.text
+    context.chat_data["order_people"].append(update.effective_user.first_name)
     edit_order_message(update, context)
 
 
@@ -46,7 +43,18 @@ def get_chat_id(update: Update, context: CallbackContext):
 def time_to_eat(context: CallbackContext):
     now = datetime.datetime.now().astimezone(pytz.timezone("Europe/Kiev"))
     if datetime.datetime.strftime(now, "%H:%M") == settings.time_to_eat and now.weekday() not in [5, 6]:
-        context.bot.send_message(
-            text="Нове замовлення\n\nРеплай на повідомлення - добавити своє замовлення",
+        message = context.bot.send_message(
+            text=settings.first_message,
+            chat_id=settings.chat_id,
+        )
+        context.dispatcher.chat_data[settings.chat_id].clear()
+        context.dispatcher.chat_data[settings.chat_id]["message_id"] = message.message_id
+        context.dispatcher.chat_data[settings.chat_id]["order"] = {}
+        context.dispatcher.chat_data[settings.chat_id]["order_people"] = []
+
+    if datetime.datetime.strftime(now, "%H:%M") == settings.time_to_order and now.weekday() not in [5, 6]:
+        user = context.dispatcher.chat_data[settings.chat_id]["order_people"][random.randint(0, len(context.dispatcher.chat_data[settings.chat_id]["order_people"]) - 1)]
+        message = context.bot.send_message(
+            text=f"Сьогодні замовляє {user}",
             chat_id=settings.chat_id,
         )
--- settings.py
+++ settings.py
@@ -1,8 +1,10 @@
 BOT_TOKEN = "5297019034:AAHhrkTclZG0qBaXV5dCnE9AvPQWDAqPC-s"
-time_to_eat = "11:30"
-chat_id = -489162852 #Datawiz BES
+time_to_eat = "10:19"
+time_to_order = "10:20"
+chat_id = -778053849 #Datawiz BES
 message_template = (
     "Замовлення\n"
     "{order}\n\n"
     "Реплай на повідомлення - добавити своє замовлення"
 )
+first_message = "Привіт. Що будем сьогодні їсти?\n\nРеплай на повідомлення - добавити своє замовлення"
